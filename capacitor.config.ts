ff4fb23df6b00d017b1cede0510e79c663b58949
woqls22 <woqls226@gmail.com>
2021-10-09 14:34:53 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lifeNcut',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
