99138dffbabd746ea366ca0a65868cd89bee71cd
woqls22 <woqls226@gmail.com>
2021-11-26 14:14:11 +0900

    [BugFix] 최근 등록한 사진이 가장 상단에 위치하도록 수정

--- src/Store/AlbumStore.tsx
+++ src/Store/AlbumStore.tsx
@@ -11,15 +11,15 @@ import { SpringAxios } from "../Utils/Utils";
 interface AlbumStore {
   AlbumList: Album[];
   ClickedAlbum: Album | null;
-  ImgFileList:ImgFile[];
+  ImgFileList: ImgFile[];
   fetchAlbumList: () => Promise<void>;
-  fetchImgsByPaging:(albumId:string, page:number)=>Promise<void>;
-  clickAlbum:(albumId:string)=>Promise<void>;
+  fetchImgsByPaging: (albumId: string, page: number) => Promise<void>;
+  clickAlbum: (albumId: string) => Promise<void>;
 }
 const AlbumStore = observable<AlbumStore>({
   AlbumList: [],
   ClickedAlbum: null,
-  ImgFileList:[],
+  ImgFileList: [],
   async fetchAlbumList() {
     const albumList = await getAlbumList();
     if (albumList) this.AlbumList = albumList;
@@ -28,21 +28,22 @@ const AlbumStore = observable<AlbumStore>({
       //못받아온 경우
     }
   },
-  async clickAlbum(albumId:string){
+  async clickAlbum(albumId: string) {
     const target = await getAlbumWithId(albumId);
-    if(target) this.ClickedAlbum=target;
-    if(!target){
-        
+    if (target) this.ClickedAlbum = target;
+    if (!target) {
     }
-  }
-  ,
-  async fetchImgsByPaging(albumId:string, page:number){
-      const imgs = await getImgsByPaging(page,albumId)
-      if (imgs) this.ImgFileList = this.ImgFileList.concat(imgs);
-      console.log(this.AlbumList);
-      if (!imgs) {
-        //못받아온 경우
-      }
+  },
+  async fetchImgsByPaging(albumId: string, page: number) {
+    const imgs = await getImgsByPaging(page, albumId);
+    if (imgs) {
+      this.ImgFileList = this.ImgFileList.concat(imgs);
+      this.ImgFileList=[...this.ImgFileList].reverse();
+    }
+    console.log(this.AlbumList);
+    if (!imgs) {
+      //못받아온 경우
+    }
   },
 });
 export default AlbumStore;
