a409a2ffff5a4a7af35a901516d7319bcd1c50a9
woqls22 <woqls226@gmail.com>
2021-12-16 10:28:18 +0900

    [BugFix] BackEnd URL, FrontEnd URL localhost로 임시설정

--- src/Utils/Constants.tsx
+++ src/Utils/Constants.tsx
@@ -1,3 +1,3 @@
-export const rootURL="http://localhost:8090";
+export const rootURL = "http://localhost:8090";
 export const FrontURL = "http://localhost:8100";
 // export const rootURL="http://133.186.241.96:8090";
